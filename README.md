44f3d8a087f1083e7f704d1706e63cfb1f2a549b
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-13 12:07:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Sale order line menu with cancel motif field
+--------------------------------------------
+This module display the cancel motif field sale order line tree/form views.
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale order line menu with cancel motif field',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale',
+    'description': "",
+    'depends': [
+        'sale_order_line_menu','stock_picking_order_line_cancel_motif'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
